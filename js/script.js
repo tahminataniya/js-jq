c22779c8382106b82d516afeb1e5e7246fe28d40
tahminataniya <110694174+tahminataniya@users.noreply.github.com>
2022-11-18 15:44:49 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function textChange(){
+	document.getElementById('p').innerHTML="hi!its me tahmina";
+}
+
+//------
+
+$(document).ready(function(){
+
+	$('.j_button').click(function(){
+		alert("hello world");
+	})
+})
+
+
+
